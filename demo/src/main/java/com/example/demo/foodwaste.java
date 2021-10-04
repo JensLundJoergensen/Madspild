220e9aef9f6792f6ac58060bfe494411eda4797f
macow-lab <mmacovv@gmail.com>
2021-10-04 11:55:52 +0200

    Basics

---
+++ demo/src/main/java/com/example/demo/foodwaste.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication
+public class foodwaste {
+
+	public static void main(String[] args) {
+		SpringApplication.run(foodwaste.class, args);
+	}
+
+	@GetMapping("/contact")
+	public String contact() {
+		// Enter template name
+		return "contact";
+	}
+
+	@GetMapping("/facts")
+	public String facts() {
+		// Enter template name
+		return "contact";
+	}
+
+}
