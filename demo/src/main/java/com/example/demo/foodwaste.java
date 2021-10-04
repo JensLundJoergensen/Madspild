5646a8a8b953a305c98b63cbbd87bb2639907d5a
macow-lab <mmacovv@gmail.com>
2021-10-04 13:46:35 +0200

    Stylesheet and templates work

--- demo/src/main/java/com/example/demo/foodwaste.java
+++ demo/src/main/java/com/example/demo/foodwaste.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @SpringBootApplication
@@ -13,10 +14,23 @@ public class foodwaste {
 		SpringApplication.run(foodwaste.class, args);
 	}
 
+	@GetMapping("/")
+	public String root() {
+		// Enter template name
+		return "root";
+	}
+
 	@GetMapping("/contact")
 	public String contact() {
 		// Enter template name
 		return "contact";
+	}
+
+	// TODO Handle POST on /contact route
+
+	@PostMapping("/contact")
+	public void postContact() {
+
 	}
 
 	@GetMapping("/facts")
