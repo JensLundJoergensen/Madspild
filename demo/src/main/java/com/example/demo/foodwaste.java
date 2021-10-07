c8da485d916aa64b65774ea1226f2a454606672a
JensC <jenschristian0002@gmail.com>
2021-10-07 12:07:28 +0200

    arbejde på database implimentation

--- demo/src/main/java/com/example/demo/foodwaste.java
+++ demo/src/main/java/com/example/demo/foodwaste.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.example.demo.Services.ContactHandler;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @SpringBootApplication
 public class foodwaste {
-
+ContactHandler uggabug = new ContactHandler();
 	public static void main(String[] args) {
 		SpringApplication.run(foodwaste.class, args);
 	}
@@ -30,6 +32,7 @@ public class foodwaste {
 
 	@PostMapping("/contact")
 	public void postContact() {
+		int result=uggabug.validateContact(firstName, lastName, email, message, address, postalCode, phoneNumber)
 
 	}
 
@@ -38,5 +41,6 @@ public class foodwaste {
 		// Enter template name
 		return "facts";
 	}
+	
 	
 }
